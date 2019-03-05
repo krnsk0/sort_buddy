ffcd1a7c052135718ef0ef21d6cb82f95850c2f4
Jon Kurinsky <jkurinsky@gmail.com>
2019-03-05 17:09:43 -0600

    add some eslint ignores and roll back work on merge function

--- merge.js
+++ merge.js
@@ -1,2 +1,7 @@
-// eslint-disable-next-line no-unused-vars
-function* merge(array) {}
+/* eslint-disable max-statements */
+/* eslint-disable no-undef */
+/* eslint-disable complexity */
+/* eslint-disable require-yield */
+/* eslint-disable no-unused-vars */
+
+function* merge(array, start = 0, end = array.length) {}
