266e5826f87f1199a4de8e372ba7c5ccd2736071
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-07 19:11:51 -0500

    finish heapsort

--- src/algos/heapSort.js
+++ src/algos/heapSort.js
@@ -1,3 +1,4 @@
+/* eslint-disable complexity */
 import { copyData } from './utils';
 
 const heapSort = input => {
@@ -8,6 +9,41 @@ const heapSort = input => {
   // make a working copy of the array we'll use in the sort
   const array = copyData(input);
 
+  const heapify = (i, max) => {
+    while (i < max) {
+      let index = i;
+      let leftChild = 2 * i + 1;
+      let rightChild = 2 * i + 2;
+
+      if (leftChild < max) {
+        if (array[leftChild].value > array[index].value) {
+          index = leftChild;
+        }
+      }
+      if (rightChild < max) {
+        if (array[rightChild].value > array[index].value) {
+          index = rightChild;
+        }
+      }
+
+      if (index === i) {
+        return;
+      }
+
+      array[i].status = 'comparing';
+      array[index].status = 'comparing';
+
+      [array[i], array[index]] = [array[index], array[i]];
+
+      history.push(copyData(array));
+
+      array[i].status = 'unsorted';
+      array[index].status = 'unsorted';
+
+      i = index;
+    }
+  };
+
   const buildMaxHeap = () => {
     let i = Math.floor(array.length / 2 - 1);
 
@@ -17,37 +53,13 @@ const heapSort = input => {
     }
   };
 
-  const heapify = (i, max) => {
-    while (i < max) {
-      let index = i;
-      let leftChild = 2 * i + 1;
-      let rightChild = 2 * i + 2;
-
-      if (leftChild < max && array[leftChild].value > array[index].value) {
-        index = leftChild;
-      }
-      if (rightChild < max && array[rightChild].value > array[index].value) {
-        index = rightChild;
-      }
-
-      if (index === i) {
-        return;
-      }
-
-      [array[i], array[index]] = [array[index], array[i]];
-
-      history.push(copyData(array));
-
-      i = index;
-    }
-  };
-
   const heapSortHelper = () => {
     buildMaxHeap();
     let lastElement = array.length - 1;
 
     while (lastElement > 0) {
       [array[0], array[lastElement]] = [array[lastElement], array[0]];
+      array[lastElement].status = 'sorted';
       history.push(copyData(array));
       heapify(0, lastElement);
       lastElement -= 1;
@@ -56,6 +68,7 @@ const heapSort = input => {
 
   heapSortHelper();
 
+  array[0].status = 'sorted';
   history.push(copyData(array));
   return history;
 };
