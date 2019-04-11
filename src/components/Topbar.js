b64fb3f9221231d69d2702f2570ab012cd41d89d
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-10 19:46:51 -0500

    add comments to topbar

--- src/components/Topbar.js
+++ src/components/Topbar.js
@@ -12,7 +12,7 @@ import {
 class disconnectedTopbar extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { size: props.size };
+    this.state = { size: props.size }; // for the controlled form element
   }
 
   componentDidMount() {
@@ -24,7 +24,16 @@ class disconnectedTopbar extends React.Component {
   }
 
   componentDidUpdate(prevProps) {
-    const { playing } = this.props;
+    const { playing, pointer, maxLength, togglePlaying } = this.props;
+
+    // if playing and have hit the max, stop
+    if (playing && pointer >= maxLength) {
+      togglePlaying();
+      clearInterval(this.interval);
+      return;
+    }
+
+    // set or clear an interval if playing flag changed
     if (playing && !prevProps.playing) {
       this.interval = setInterval(this.props.stepForward, 100);
     } else if (!playing && prevProps.playing) {
@@ -34,7 +43,7 @@ class disconnectedTopbar extends React.Component {
 
   keyHandler = evt => {
     const { stepBack, stepForward, togglePlaying } = this.props;
-
+    // arrow key presses will stop playing if playing
     if (evt.keyCode === 39) {
       if (this.props.playing) togglePlaying();
       stepForward();
@@ -42,18 +51,21 @@ class disconnectedTopbar extends React.Component {
       if (this.props.playing) togglePlaying();
       stepBack();
     } else if (evt.keyCode === 32) {
+      evt.preventDefault(); // prevents spacebar from activating the previously selected button
       togglePlaying();
     }
   };
 
+  // runs when the select element changes
   onSizeChange = evt => {
     this.setState({ size: evt.target.value });
   };
 
-  onReset(evt) {
-    evt.preventDefault();
-    if (this.props.playing) togglePlaying();
-    this.props.reset(this.state.size);
+  onReset(evt, props) {
+    evt.preventDefault(); // stops form submission and page refresh
+    const { playing, reset } = props;
+    if (playing) togglePlaying(); // stops playing if it's playing
+    reset(this.state.size);
   }
 
   render() {
@@ -63,27 +75,32 @@ class disconnectedTopbar extends React.Component {
           <span className="title">sort_buddy</span>
         </span>
         <span className="topbar-container">
-          {!this.props.playing && this.props.pointer > 0 && (
-            <button
-              type="button"
-              onClick={this.props.stepBack}
-              className="topbar-button"
-            >
-              {'<<'}
-            </button>
-          )}
+          <button
+            type="button"
+            onClick={evt => {
+              // stops playing if playing
+              if (this.props.playing) this.props.togglePlaying();
+              this.props.stepBack();
+            }}
+            className="topbar-button"
+          >
+            {'<<'}
+          </button>
+
           <span className="topbar-text">
             {this.props.pointer} / {this.props.maxLength}
           </span>
-          {!this.props.playing && this.props.pointer < this.props.maxLength && (
-            <button
-              type="button"
-              onClick={this.props.stepForward}
-              className="topbar-button"
-            >
-              {'>>'}
-            </button>
-          )}
+          <button
+            type="button"
+            onClick={evt => {
+              // stops playing if playing
+              if (this.props.playing) this.props.togglePlaying();
+              this.props.stepForward();
+            }}
+            className="topbar-button"
+          >
+            {'>>'}
+          </button>
         </span>
         <span className="topbar-container">
           <button
@@ -91,9 +108,15 @@ class disconnectedTopbar extends React.Component {
             onClick={this.props.togglePlaying}
             className="topbar-button"
           >
-            {this.props.playing ? 'pause' : 'play'}
+            {this.props.playing ? 'stop' : 'play'}
           </button>
-          <form onSubmit={this.onReset} id="size-form">
+          <form
+            onSubmit={evt => {
+              // have to pass in props to put them in scope
+              this.onReset(evt, this.props);
+            }}
+            id="size-form"
+          >
             <button type="submit" className="topbar-button">
               reset
             </button>
@@ -101,6 +124,7 @@ class disconnectedTopbar extends React.Component {
               id="size-selector"
               className="size-selector"
               onChange={this.onSizeChange}
+              // taken from the initial state in redux store
               value={this.state.size}
             >
               <option value="8">8</option>
