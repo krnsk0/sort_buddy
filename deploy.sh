614c4dde95fd73eda14e430e6eb7ba362be12bb7
Jon Kurinsky <jkurinsky@gmail.com>
2019-04-14 16:17:52 -0500

    get rid of deploy folder

--- deploy.sh
+++ deploy.sh
@@ -5,27 +5,18 @@ set -e
 
 # return to master
 function cleanup_at_exit {
-#  git checkout master
-  rm -rf deploy
 }
 trap cleanup_at_exit EXIT
 
-# create deploy folder and copy things in
-cp -rf public deploy
-rm deploy/bundle.js
-rm deploy/bundle.js.map
-
 # run webpack in production mode
-webpack --mode=production -o deploy/bundle.js
-
-# checkout the pages branch
-# git checkout gh-pages
+webpack --mode=production
 
 # force add the bundle
-git add -f public/bundle.js public/bundle.js.map
+git add -f public/bundle.js
+git add -f public/bundle.js.map
 
 # create a commit even if no changes
 git commit --allow-empty -m 'Deploying'
 
 # push to github
-git subtree push --prefix deploy origin gh-pages
+git subtree push --prefix public origin gh-pages
